1ff6594c53314e05c6aaecbb9e8f701d9495f3d2
nenekelvin <karvinmitchar@gmail.com>
2020-05-20 17:30:11 +0300

    renamed asset pieline

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,7 +8,8 @@ class JobsController < ApplicationController
 end
 
 def create
-@job = Job.new(params.require(:job).permit(:title, :company, :url))  if @job.save
+@job = Job.new(params.require(:job).permit(:title, :company, :url)) 
+ if @job.save
     redirect_to root_path
   else
     render "new"
